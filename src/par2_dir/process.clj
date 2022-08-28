2b3adb3fcb2b25689af71b21df78f0b4270141a9
Arkadii Drovosekov <arkdro@protonmail.com>
2022-08-28 13:08:47 +0200

    wip

--- src/par2_dir/process.clj
+++ src/par2_dir/process.clj
@@ -1,4 +1,3 @@
-(ns par2-dir.process)
 (ns par2-dir.process
   (:require [clojure.string :as str]))
 
@@ -26,10 +25,36 @@
       (str/starts-with? dir sanitized) (subs dir sanitized_length)
       :else dir)))
 
+(defn build_new_output_dir
+  [outdir to_delete file]
+  (let [input_file_dir (get_input_file_directory file)
+        shortened_input_path (delete_part input_path to_delete)
+        shortened_input_dir (get_dir shortened_input_path)
+      ;  output_dir (create_full_output_dir)
+        ]
+    )
+  )
+
+(defn get_input_files
+  [indir]
+  (filter #(.isFile %)
+          (file-seq (clojure.java.io/file indir))))
+
+(defn process_one_file
+  [outdir to_delete file]
+  (let [new_output_dir (build_new_output_dir outdir to_delete file)
+        link (create_link new_output_dir file)]
+    (call_par2 link)
+    (remove_link link)
+    )
+  )
+
+(defn process_files
+  [outdir to_delete files]
+  (for [file files]
+    (process_one_file outdir to_delete file)))
 
 (defn process_input_dir
   [indir outdir to_delete]
-  (println indir)
-  (println outdir)
-  (println to_delete)
-  )
+  (let [files (get_input_files indir)]
+    (process_files outdir to_delete files)))
