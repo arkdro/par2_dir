ef762d8e2c513293ae8341baacfb5f5a15e98e96
Arkadii Drovosekov <arkdro@protonmail.com>
2022-08-28 13:08:30 +0200

    add aux functions

--- src/par2_dir/process.clj
+++ src/par2_dir/process.clj
@@ -1,4 +1,31 @@
 (ns par2-dir.process)
+(ns par2-dir.process
+  (:require [clojure.string :as str]))
+
+(def root "/")
+
+(defn get_input_file_directory
+  [file]
+  ;; FIXME handle the root directory: '/'
+  (.getParent file))
+
+(defn sanitize_to_delete
+  [dir]
+  (let [without_end (str/replace dir #"/+$" "")]
+    (if (str/blank? without_end)
+      root
+      without_end)))
+
+(defn delete_part
+  [dir to_delete]
+  ;; FIXME handle unicode
+  (let [sanitized (sanitize_to_delete to_delete)
+        sanitized_length (count sanitized)]
+    (cond
+      (= sanitized root) dir
+      (str/starts-with? dir sanitized) (subs dir sanitized_length)
+      :else dir)))
+
 
 (defn process_input_dir
   [indir outdir to_delete]
